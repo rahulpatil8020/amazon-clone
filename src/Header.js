ebbd937608a386ad88dd60d3d019ddd1db44de4c
rahulpatil8020 <rahuldpatil20@gmail.com>
2021-07-22 10:48:26 +0530

    ....

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
 function Header() {
+  const [{basket}, dispatch] = useStateValue();
   return (
     <div className='header'>
-      <img
-        className='header_logo'
-        src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
-        alt='My Image'
-      />
+      <Link to="/">
+        <img
+          className='header_logo'
+          src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+          alt='My Image'
+        />
+      </Link>
       <div className='header_search'>
         <input type='text' className='header_searchInput' />
         <SearchIcon className='header_searchIcon' />
@@ -31,10 +36,12 @@ function Header() {
           <span className='header_optionLineTwo'>Prime</span>
         </div>
 
-        <div className='header_optionBasket'>
-          <ShoppingBasketIcon />
-          <span className='header_optionLineTwo header_basketCount'>0</span>
-        </div>
+        <Link to="/checkout">
+          <div className='header_optionBasket'>
+            <ShoppingBasketIcon />
+            <span className='header_optionLineTwo header_basketCount'>{basket?.length}</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
