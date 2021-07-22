5646a74994dc1a50fd447e448d736bcb3f4ae842
rahulpatil8020 <rahuldpatil20@gmail.com>
2021-07-22 10:43:10 +0530

    adding products to baskets

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,24 @@
 import './Product.css';
 import React from 'react';
 import StarIcon from '@material-ui/icons/Star';
+import { useStateValue } from './StateProvider';
 
 function Product({ id, title, image, price, rating }) {
+  
+  const [{basket}, dispatch] = useStateValue();
+  
+  const addToBasket = ()  => {
+      dispatch({
+        type: "ADD_TO_BASKET",
+        item: {
+          id:id,
+          title:title,
+          image: image,
+          price: price,
+          rating: rating
+        }
+      })
+    }
   return (
     <div className='product'>
       <div className='product_info'>
@@ -22,7 +38,7 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt='' />
-      <button>Add To Basket</button>
+      <button onClick={addToBasket}>Add To Basket</button>
     </div>
   );
 }
