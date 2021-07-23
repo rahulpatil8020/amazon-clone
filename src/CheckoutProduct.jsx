6b4760bc0b847737fc269b9ddc0c4abbf8e64b6b
rahulpatil8020 <rahuldpatil20@gmail.com>
2021-07-23 16:29:03 +0530

    added count to checkout product and changing it according to the input button

--- src/CheckoutProduct.jsx
+++ src/CheckoutProduct.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
-import './CheckoutProduct.css'
+import React, {useState} from 'react';
+import './CheckoutProduct.css';
 import StarIcon from '@material-ui/icons/Star';
 import { useStateValue } from './StateProvider';
 
+function CheckoutProduct({id, image, title, price, rating, count}) {
 
-function CheckoutProduct({id, image, title, price, rating}) {
+    const [itemCount, setItemCount] = useState(count);
+
+    const addItem = () => {
+        setItemCount(itemCount + 1)
+        console.log(itemCount)
+        updateItemCount();
+    }
+    const subItem = () => {       
+        setItemCount(itemCount - 1);    
+        console.log(itemCount)  
+        if (itemCount < 1){
+            removeFromBasket();
+        }
+        else {
+            updateItemCount();
+        }
+    }
 
     const [{basket}, dispatch] = useStateValue();
+q   `adfswASDFWASDFWASDFWADF`
+    const updateItemCount = () => {
+        dispatch({
+            type: "CHANGE_ITEM_COUNT", 
+            id:id,
+            count: itemCount,
+        });
+    }
+
     const removeFromBasket = () =>{
         // remove item from basket
         dispatch({
             type: "REMOVE_FROM_BASKET",
             id: id,
         })
-
     }
-
     return (
         <div className="checkout__product">
             <img className="checkout__product__image" src={image} />
@@ -40,9 +64,14 @@ function CheckoutProduct({id, image, title, price, rating}) {
                             </p>
                         ))}
                 </div>
-                <button onClick={removeFromBasket}>
-                    Remove From Basket
+                <strong>{itemCount}</strong>
+                <div className="checkout__buttons">
+                <button className="item__count" onClick={subItem}>-</button>
+                <button className="remove__all" onClick={removeFromBasket}>
+                    Remove All Basket
                 </button>
+                <button className="item__count" onClick={addItem}>+</button>
+                </div>
             </div>
         </div>
 
